315e798e446f2606728ed7f2410612b69f7e02cf
DaBriNan <138089746+DaBriNan@users.noreply.github.com>
2025-07-11 22:49:45 -0600

    ajuste de columnas en speakers

--- src/components/Speakers.vue
+++ src/components/Speakers.vue
@@ -217,7 +217,7 @@ const getIconColor = (socialName: string) => {
     </div>
 
     <div
-      class="flex justify-center" 
+      class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8" 
       
     >
       <Card
